912cfc7f901c2522c33729207cd6690f91610eb7
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2023-06-28 14:11:29 +0100

    feat: add product app, models & fixtures

--- core/settings.py
+++ core/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount.providers.google',
     'allauth.socialaccount.providers.apple',
     'apps.home',
+    'apps.products',
 ]
 
 # Provider specific settings
