036702611509ccf5e3e6bef0d437b967c31e31b2
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2023-06-29 14:35:33 +0100

    feat: add product detail view and URL

--- apps/products/urls.py
+++ apps/products/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from .views import AllProductsView
+from .views import AllProductsView, ProductDetailView
 
 urlpatterns = [
     path('', AllProductsView.as_view(), name='products'),
+    path('<product_id>/', ProductDetailView.as_view(), name='product_detail'),
+
 ]
--- apps/products/views.py
+++ apps/products/views.py
@@ -1,9 +1,11 @@
 from .models import Product
 from django.views.generic import TemplateView
+from django.shortcuts import render, get_object_or_404
 # Create your views here.
 
 
 class AllProductsView(TemplateView):
+    """ A view to show all products, including sorting and search queries"""
     template_name = 'products.html'
 
     def get_context_data(self, **kwargs):
@@ -11,3 +13,13 @@ class AllProductsView(TemplateView):
         context['products'] = Product.objects.all()
         return context
 
+
+class ProductDetailView(TemplateView):
+    """ A view to show individual product details """
+    template_name = 'product_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        product = get_object_or_404(Product, pk=kwargs['product_id'])
+        context['product'] = product
+        return context
