0c5602cdd42299648268ed1f801363faa03835ca
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2023-06-29 11:19:18 +0100

    feat: add media & root urls

--- core/settings.py
+++ core/settings.py
@@ -172,6 +172,9 @@ STATICFILES_DIRS = [DJANGO_VITE_ASSETS_PATH,
                     BASE_DIR / 'static/assets']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
