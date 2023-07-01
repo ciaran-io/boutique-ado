b069e6df6bb6e34d8697b623a527e343ec86add2
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2023-07-01 13:31:05 +0100

    feat: add cart app urls & views, update core urls

--- core/urls.py
+++ core/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('', include('apps.home.urls')),
     path('products/', include('apps.products.urls')),
+    path('cart/', include('apps.cart.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
