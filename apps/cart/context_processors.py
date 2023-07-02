2f86dea3b94b8a0b69d7272056cadc8bc85f997d
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2023-07-02 13:38:01 +0100

    feat: update context processors
    1. Add product, product id & product quantity to cart items.

--- apps/cart/context_processors.py
+++ apps/cart/context_processors.py
@@ -1,10 +1,23 @@
 from core import settings
+from django.shortcuts import get_object_or_404
+from apps.products.models import Product
 
 
 def cart_contents(request):
     cart_items = []
     total = 0
     product_count = 0
+    cart = request.session.get('cart', {})
+
+    for item_id, quantity in cart.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        cart_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = settings.STANDARD_DELIVERY_COST
