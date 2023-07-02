d2a1ad7aa741efa38b26700df21e78e2ac26806d
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2023-07-02 12:13:07 +0100

    feat: add (add to a cart) url

--- apps/cart/urls.py
+++ apps/cart/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import CartPageView
+from .utils import add_to_cart
 
 urlpatterns = [
     path('', CartPageView.as_view(), name='cart'),
+    path('add/<item_id>/', add_to_cart, name='add_to_cart'),
 ]
