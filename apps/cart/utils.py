f4acfbe0a1be1d3329534c476197830983062017
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2023-07-19 12:13:29 +0100

    feat: Adjust product items in cart
    1 Adjust item in cart, send success/fail message and status code.
    2. Remove item from cart if quantity is less than 1.

--- apps/cart/utils.py
+++ apps/cart/utils.py
@@ -1,4 +1,7 @@
-from django.shortcuts import redirect, reverse
+from django.shortcuts import redirect, reverse, HttpResponse, get_object_or_404
+from django.contrib import messages
+
+from apps.products.models import Product
 
 
 def add_to_cart(request, item_id):
@@ -60,6 +63,44 @@ def remove_from_cart(request, item_id):
         messages.error(request, f'Error removing item: {e}')
         return HttpResponse(status=500)
 
+
+def adjust_cart(request, item_id):
+    """ Adjust the quantity of the
+    specified product to the specified amount """
+
+    product = get_object_or_404(Product, pk=item_id)
+    quantity = int(request.POST.get('quantity'))
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
+
+    cart = request.session.get('cart', {})
+
+    if size:
+        if quantity > 0:
+            cart[item_id]['items_by_size'][size] = quantity
+            messages.success(request,
+                             f'Updated size {size.upper()} {product.name}'
+                             f' quantity to '
+                             f'{cart[item_id]["items_by_size"][size]}')
+        else:
+            del cart[item_id]['items_by_size'][size]
+            if not cart[item_id]['items_by_size']:
+                cart.pop(item_id)
+            messages.success(request,
+                             f'Removed size {size.upper()}'
+                             f' {product.name} from your cart')
+    else:
+        if quantity > 0:
+            cart[item_id] = quantity
+            messages.success(request, f'Updated {product.name}'
+                                      f' quantity to {cart[item_id]}')
+        else:
+            cart.pop(item_id)
+            messages.success(request, f'Removed {product.name} from your cart')
+
+    request.session['cart'] = cart
+    return redirect(reverse('cart'))
 
     else:
         if size:
