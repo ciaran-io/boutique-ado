99e0707c3c10cd77984a20d14a51a8e96e06e9a1
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2023-07-19 12:20:25 +0100

    feat: send a success message when cart updated

--- apps/cart/utils.py
+++ apps/cart/utils.py
@@ -7,6 +7,7 @@ from apps.products.models import Product
 def add_to_cart(request, item_id):
     """ Add a quantity of the specified product to the shopping cart """
 
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     size = None
@@ -15,20 +16,32 @@ def add_to_cart(request, item_id):
 
     cart = request.session.get('cart', {})
 
-    if item_id in cart:
     if size:
         if item_id in list(cart.keys()):
             if size in cart[item_id]['items_by_size'].keys():
                 cart[item_id]['items_by_size'][size] += quantity
+                messages.success(request,
+                                 f'Updated size {size.upper()} '
+                                 f'{product.name} quantity to '
+                                 f'{cart[item_id]["items_by_size"][size]}')
             else:
                 cart[item_id]['items_by_size'][size] = quantity
+                messages.success(request,
+                                 f'Added size {size.upper()} '
+                                 f'{product.name} to your cart')
         else:
             cart[item_id] = {'items_by_size': {size: quantity}}
+            messages.success(request, f'Added size {size.upper()}'
+                                      f' {product.name} to your cart')
     else:
         if item_id in list(cart.keys()):
             cart[item_id] += quantity
+            messages.success(request,
+                             f'Updated {product.name} quantity to '
+                             f'{cart[item_id]}')
         else:
             cart[item_id] = quantity
+            messages.success(request, f'Added {product.name} to your cart')
 
     request.session['cart'] = cart
     return redirect(redirect_url)
