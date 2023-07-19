cf59296e8eff11abd3dd4ea7c9f466d24547d421
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2023-07-19 11:58:07 +0100

    feat: add urls for adjusting, removing care items

--- apps/cart/urls.py
+++ apps/cart/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import CartPageView
-from .utils import add_to_cart
+from .utils import add_to_cart, adjust_cart, remove_from_cart
 
 urlpatterns = [
     path('', CartPageView.as_view(), name='cart'),
     path('add/<item_id>/', add_to_cart, name='add_to_cart'),
+    path('adjust/<item_id>/', adjust_cart, name='adjust_cart'),
+    path('remove/<item_id>/', remove_from_cart, name='remove_from_cart'),
 ]
