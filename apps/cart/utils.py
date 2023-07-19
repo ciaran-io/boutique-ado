a55c14bc8306a57f0143c7a4b799a8c457626379
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2023-07-19 12:10:35 +0100

    feat: remove item from cart
    1 Remove item from cart, send success/fail message and status code.

--- apps/cart/utils.py
+++ apps/cart/utils.py
@@ -31,6 +31,36 @@ def add_to_cart(request, item_id):
     return redirect(redirect_url)
 
 
+def remove_from_cart(request, item_id):
+    """ Remove the item from the shopping cart """
+
+    try:
+        product = get_object_or_404(Product, pk=item_id)
+        size = None
+        if 'product_size' in request.POST:
+            size = request.POST['product_size']
+
+        cart = request.session.get('cart', {})
+
+        if size:
+            del cart[item_id]['items_by_size'][size]
+            if not cart[item_id]['items_by_size']:
+                cart.pop(item_id)
+            messages.success(request,
+                             f'Removed size {size.upper()}'
+                             f' {product.name} from your bag')
+        else:
+            cart.pop(item_id)
+            messages.success(request, f'Removed {product.name} from your cart')
+
+        request.session['cart'] = cart
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
+        return HttpResponse(status=500)
+
+
     else:
         if size:
             cart[item_id] = {'items_by_size': {size: quantity}}
