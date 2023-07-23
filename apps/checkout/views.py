2e97aee4aa14f062e0bb28654e7472c9dbb6082b
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2023-07-23 15:53:06 +0100

    feat: add checkout post method & success view
    1. Handle success/fail post-method for user payments on checkout.
    2. Add success checkout view to display customer or order &
    customer details.

--- apps/checkout/views.py
+++ apps/checkout/views.py
@@ -45,3 +45,88 @@ class CheckoutView(TemplateView):
         })
 
         return context
+
+    def post(self, request, *args, **kwargs):
+
+        order_form = OrderForm(request.POST)
+        if order_form.is_valid():
+            order = order_form.save(commit=False)
+            order.save()
+            #
+            cart = self.request.session.get('cart', {})
+            for item_id, item_data in cart.items():
+                try:
+                    product = Product.objects.get(id=item_id)
+                    if isinstance(item_data,
+                                  int):  # if item_data is an integer
+                        order_line_item = OrderLineItem(
+                            order=order,
+                            product=product,
+                            quantity=item_data,
+                        )
+                        order_line_item.save()
+                    else:  # if item_data is a dictionary
+                        for size, quantity in \
+                                item_data['items_by_size'].items():
+                            order_line_item = OrderLineItem(
+                                order=order,
+                                product=product,
+                                quantity=quantity,
+                                product_size=size,
+                            )
+                            order_line_item.save()
+                except Product.DoesNotExist:
+                    messages.error(request, (
+                        "One of the products in your cart wasn't found in "
+                        "our database."
+                        "Please call us for assistance!")
+                                   )
+                    order.delete()
+                    return redirect(reverse('view_cart'))
+
+            request.session['save_info'] = 'save-info' in request.POST
+            return redirect(
+                reverse('checkout_success', args=[order.order_number]))
+
+        else:
+            messages.error(request, 'There was an error with your form. \
+                Please double check your information.')
+            return redirect(reverse('checkout'))
+
+
+class CheckoutSuccessView(TemplateView):
+    template_name = 'checkout_success.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        order_number = kwargs.get('order_number')
+        order = get_object_or_404(Order, order_number=order_number)
+        messages.success(self.request, f'Order successfully processed! \
+            Your order number is {order_number}. A confirmation \
+            email will be sent to {order.email}.')
+        if 'cart' in self.request.session:
+            del self.request.session['cart']
+        customer_details = {
+            'full_name': order.full_name,
+            'email': order.email,
+            'phone_number': order.phone_number,
+            'country': order.country,
+            'postcode': order.postcode,
+            'town_or_city': order.town_or_city,
+            'street_address1': order.street_address1,
+            'street_address2': order.street_address2,
+            'county': order.county,
+        }
+        # add N/a for empty fields
+        for field, value in customer_details.items():
+            if not value:
+                customer_details[field] = 'N/a'
+        # remove _ from field names
+        customer_details = {k.replace('_', ' ').title(): v for k, v in
+                            customer_details.items()}
+
+        context.update({
+            'order': order,
+            'customer_details': customer_details,
+        })
+        return context
