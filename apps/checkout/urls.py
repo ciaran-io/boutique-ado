5e368d8629e5dd08d8142a92c687621472d40eea
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2023-07-23 19:49:03 +0100

    feat: add stipe webhook response methods & url

--- apps/checkout/urls.py
+++ apps/checkout/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
 from .views import CheckoutSuccessView, CheckoutView
+from .webhooks import webhook
 
 urlpatterns = [
     path('', CheckoutView.as_view(), name='checkout'),
     path('checkout_success/<order_number>', CheckoutSuccessView.as_view(),
          name='checkout_success'),
+    path('wh/', webhook, name='webhook'),
 ]
