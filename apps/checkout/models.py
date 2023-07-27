134ad7e82f82de8ce1bdba0e79c83c45ca3bb073
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2023-07-27 14:40:06 +0100

    feat: Add new migrations, modify checkout models
    1.New migration files were added for the `profiles` and `checkout` apps
    to update their models. This includes creating the UserProfile model &
    adding new fields to the Order model for stripe PID and original cart.
    
    2. Modifications were made to the `checkout` app to accommodate changes
    in the order management system. The Order model now uses CountryField
    instead of CharField for the 'country' field, and it now includes
    'stripe_pid' and 'original_cart'. The `views.py` file in the `checkout`
    app saves 'stripe_pid' and 'original_cart' when an order is created.
    
    These changes improve the checkout process and enhance order tracking by
    including the stripe payment ID and the state of the cart at the time of
    the order. The checkout admin view now displays 'original_cart' and
    'stripe_pid', and the checkout form no longer includes 'country' in its
    placeholders.
    
    3.Aesthetic improvements were made to `checkout.html`,
    including the option to save delivery information for authenticated
    users.
    
    summary: these updates make the checkout process more reliable and
    improve the user experience, particularly for registered users.

--- apps/checkout/models.py
+++ apps/checkout/models.py
@@ -3,8 +3,10 @@ import uuid
 from django.conf import settings
 from django.db import models
 from django.db.models import Sum
+from django_countries.fields import CountryField
 
 from apps.products.models import Product
+from apps.profiles.models import UserProfile
 
 
 # Create your models here.
@@ -18,10 +20,14 @@ def _generate_order_number():
 
 class Order(models.Model):
     order_number = models.CharField(max_length=32, null=False, editable=False)
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
+                                     null=True, blank=True,
+                                     related_name='orders')
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
-    country = models.CharField(max_length=40, null=False, blank=False)
+    country = CountryField(blank_label='Country *',
+                           null=False, blank=False)
     postcode = models.CharField(max_length=20, null=True, blank=True)
     town_or_city = models.CharField(max_length=40, null=False, blank=False)
     street_address1 = models.CharField(max_length=80, null=False, blank=False)
@@ -34,6 +40,9 @@ class Order(models.Model):
                                       null=False, default=0)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2,
                                       null=False, default=0)
+    original_cart = models.TextField(null=False, blank=False, default='')
+    stripe_pid = models.CharField(max_length=254, null=False, blank=False,
+                                  default='')
 
     def update_total(self):
         """
--- apps/checkout/views.py
+++ apps/checkout/views.py
@@ -1,3 +1,5 @@
+import json
+
 import stripe
 from django.conf import settings
 from django.contrib import messages
@@ -51,9 +53,11 @@ class CheckoutView(TemplateView):
         order_form = OrderForm(request.POST)
         if order_form.is_valid():
             order = order_form.save(commit=False)
-            order.save()
-            #
             cart = self.request.session.get('cart', {})
+            pid = request.POST.get('client_secret').split('_secret')[0]
+            order.stripe_pid = pid
+            order.original_cart = json.dumps(cart)
+            order.save()
             for item_id, item_data in cart.items():
                 try:
                     product = Product.objects.get(id=item_id)
