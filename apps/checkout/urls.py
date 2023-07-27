d5824fe3a69a4b90b9d10dbee585d1e531841298
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2023-07-27 16:31:37 +0100

    feat: add url for caching checkout data
    A new url has been added to urls.py in the checkout app. This url
    (cache_checkout_data/) is linked to the cache_checkout_data function
    from the utils module. The purpose of this change is to add metadata to
    the stripe payment intent during checkout.

--- apps/checkout/urls.py
+++ apps/checkout/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 
+from .utils import cache_checkout_data
 from .views import CheckoutSuccessView, CheckoutView
 from .webhooks import webhook
 
@@ -8,4 +9,6 @@ urlpatterns = [
     path('checkout_success/<order_number>', CheckoutSuccessView.as_view(),
          name='checkout_success'),
     path('wh/', webhook, name='webhook'),
+    path('cache_checkout_data/', cache_checkout_data,
+         name='cache_checkout_data'),
 ]
