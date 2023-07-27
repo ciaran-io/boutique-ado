7b62efe7adaa47c221fa01f1c9591d364da516d1
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2023-07-27 14:26:38 +0100

    feat: add product management features
    1. Create templates for adding and editing products in the product app.
    Update product views to include product creation, update and deletion.
    2. Restrict these views to superusers only. Add the respective urls for
    these views. This is done to allow admin users to manage products
    directly from the website.

--- apps/products/urls.py
+++ apps/products/urls.py
@@ -1,9 +1,20 @@
-from django.contrib import admin
 from django.urls import path
-from .views import AllProductsView, ProductDetailView
+
+from .views import (AllProductsView,
+                    ProductCreateView,
+                    ProductDeleteView,
+                    ProductDetailView,
+                    ProductUpdateView,
+                    )
 
 urlpatterns = [
     path('', AllProductsView.as_view(), name='products'),
-    path('<product_id>/', ProductDetailView.as_view(), name='product_detail'),
-
+    path('<int:product_id>/', ProductDetailView.as_view(),
+         name='product_detail'),
+    path('new/', ProductCreateView.as_view(),
+         name='product_create'),
+    path('update/<int:pk>/', ProductUpdateView.as_view(),
+         name='product_update'),
+    path('delete/<int:product_id>/', ProductDeleteView.as_view(),
+         name='product_delete'),
 ]
--- apps/products/views.py
+++ apps/products/views.py
@@ -1,10 +1,14 @@
 from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models import Q
 from django.db.models.functions import Lower
 from django.shortcuts import get_object_or_404, redirect, reverse
-from django.views.generic import TemplateView
+from django.views import View
+from django.views.generic import CreateView, ListView, \
+    TemplateView, UpdateView
 
-from .models import Product, Category
+from .forms import ProductForm
+from .models import Category, Product
 
 # Sort options for products
 sort_options = {
@@ -21,64 +25,73 @@ sort_options = {
 sizes = ['xs', 's', 'm', 'l', 'xl']
 
 
-class AllProductsView(TemplateView):
-    """ A view to show all products, including sorting and search queries"""
+class AllProductsView(ListView):
+    """A view to show all products, including sorting and search queries"""
     template_name = 'products.html'
+    model = Product
+    context_object_name = 'products'
+    paginate_by = 10
 
-    def get(self, request, *args, **kwargs):
-        query = request.GET.get('q')
-        category = request.GET.get('category')
-        categories = None
-        sort = None
-        direction = None
-        products = Product.objects.all()
-        search_results = True  # Flag to indicate if any search results found
+    def get_queryset(self):
+        queryset = super().get_queryset()
 
-        # If there is a query, that is sort my name
-        if sortkey := request.GET.get('sort'):
-            sort = sortkey
-            if sortkey == 'name':
-                sortkey = 'lower_name'
-                products = products.annotate(lower_name=Lower('name'))
+        query = self.request.GET.get('q')
+        category = self.request.GET.get('category')
+        sort = self.request.GET.get('sort')
+        direction = self.request.GET.get('direction')
 
-            if sortkey == 'category':
-                sortkey = 'category__name'
-
-            if direction := request.GET.get('direction'):
-                direction = direction
-                if direction == 'desc':
-                    sortkey = f'-{sortkey}'
-            products = products.order_by(sortkey)
-
-        if category and category != '':
-            categories = request.GET.get('category').split(',')
-            products = products.filter(category__name__in=categories)
-            categories = Category.objects.filter(name__in=categories)
-
-        if query and query != '':
-            products = products.filter(
+        if query:
+            queryset = queryset.filter(
                 Q(name__icontains=query) | Q(description__icontains=query)
             )
-            search_results = bool(
-                products)  # Set flag based on the presence of search results
-            if not search_results:
-                messages.error(request,
-                               "No products found for the search criteria.")
 
-                return redirect(reverse('products'))
+        if category and category != '':
+            categories = category.split(',')
+            queryset = queryset.filter(category__name__in=categories)
 
-        current_sorting = f'{sort}_{direction}'
+        if sort:
+            if sort == 'name':
+                sort = 'lower_name'
+                queryset = queryset.annotate(lower_name=Lower('name'))
+            elif sort == 'category':
+                sort = 'category__name'
+            else:
+                sort = None
 
-        context = self.get_context_data(
-            products=products,
-            search_term=query,
-            search_results=search_results,
-            current_categories=categories,
-            current_sorting=current_sorting,
-            sort_options=sort_options,
-            sizes=sizes
-        )
-        return self.render_to_response(context)
+        if direction == 'desc':
+            sort = f'-{sort}' if sort else None
+
+        if sort:
+            queryset = queryset.order_by(sort)
+
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        query = self.request.GET.get('q')
+        category = self.request.GET.get('category')
+        sort = self.request.GET.get('sort')
+        direction = self.request.GET.get('direction')
+
+        context['search_term'] = query
+        context['search_results'] = bool(self.get_queryset())
+        context['current_categories'] = \
+            Category.objects.filter(name__in=category.split(
+                ',')) if category and category != '' else None
+        context['current_sorting'] = \
+            f'{sort}_{direction}' if sort and direction else None
+        context['sort_options'] = sort_options
+        context['sizes'] = sizes
+
+        return context
+
+    def render_to_response(self, context, **response_kwargs):
+        if not context['search_results']:
+            messages.error(self.request,
+                           "No products found for the search criteria.")
+            return redirect(reverse('products'))
+
+        return super().render_to_response(context, **response_kwargs)
 
 
 class ProductDetailView(TemplateView):
@@ -91,3 +104,74 @@ class ProductDetailView(TemplateView):
         context['product'] = product
         context['sizes'] = sizes
         return context
+
+
+class ProductCreateView(LoginRequiredMixin, CreateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'product_add.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_superuser:
+            messages.error(request, 'Sorry, only store owners can do that.')
+            return redirect(reverse('home'))
+        return super().dispatch(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        messages.success(self.request, 'Successfully added product!')
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.error(self.request,
+                       'Failed to add product. '
+                       'Please ensure the form is valid.')
+        return super().form_invalid(form)
+
+    def get_success_url(self):
+        return reverse('product_detail', args=[self.object.pk])
+
+
+class ProductUpdateView(LoginRequiredMixin, UpdateView):
+    model = Product
+    template_name = 'product_edit.html'
+    form_class = ProductForm
+    context_object_name = 'product'
+
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_superuser:
+            messages.error(request, 'Sorry, only store owners can do that.')
+            return redirect(reverse('home'))
+        return super().dispatch(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        messages.success(self.request, 'Successfully updated product!')
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.error(self.request, 'Failed to update product. '
+                                     'Please ensure the form is valid.')
+        return super().form_invalid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        messages.info(self.request, f'You are editing {self.object.name}')
+        return context
+
+    def get_success_url(self):
+        return reverse('product_detail', args=[self.object.pk])
+
+
+class ProductDeleteView(LoginRequiredMixin, View):
+    """ A view to delete a product """
+
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_superuser:
+            messages.error(request, 'Sorry, only store owners can do that.')
+            return redirect(reverse('home'))
+        return super().dispatch(request, *args, **kwargs)
+
+    def get(self, request, product_id):
+        product = get_object_or_404(Product, pk=product_id)
+        product.delete()
+        messages.success(request, 'Product deleted!')
+        return redirect(reverse('products'))
