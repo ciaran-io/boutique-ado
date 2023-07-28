f6970842c418f557cb01f5a33be48e3559573829
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2023-07-28 13:27:53 +0100

    fix: django vite asset url

--- core/settings.py
+++ core/settings.py
@@ -228,7 +228,7 @@ if 'USE_AWS' in os.environ:
     # Override static and media URLs in production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
-    DJANGO_VITE_STATIC_URL_PREFIX = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}'  # noqa: E5
+    # DJANGO_VITE_STATIC_URL_PREFIX = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}'  # noqa: E5
 
 # stripe settings
 FREE_DELIVERY_THRESHOLD = 50
